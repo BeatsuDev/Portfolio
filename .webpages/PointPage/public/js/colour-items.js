54accf83716414fb2fdacbe5117e9edb9c1d75dd
Eric <ericbieszczad@gmail.com>
2021-02-12 13:02:12 +0100

    Added some styling - work on styling items better

---
+++ .webpages/PointPage/public/js/colour-items.js
@@ -0,0 +1,17 @@
+var items = document.querySelectorAll('.item');
+
+items.forEach(item => {
+	switch (/* Get name of coding language used in item*/) {
+		case 'javascript':
+			/* Change background colour to JavaScript-yellow */
+			break
+
+		case 'nodejs':
+			/* Change to nodejs green */
+			break
+
+		case 'python':
+			/* Change to Python blue */
+			break
+	}
+});
